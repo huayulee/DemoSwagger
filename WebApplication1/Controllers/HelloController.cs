c3c63bf8a067504b10043cf85ea658da4dbbe46b
Huayu Lee <huayu.lee@gmail.com>
2017-10-23 23:33:51 +0800

    first

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloController : ApiController
+    {
+        /// <summary>
+        /// 阿華田
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Baby()
+        {
+            return "Hello baby!!";
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+
+namespace WebApplication1.Controllers
+{
+    /// <summary>
+    /// Home
+    /// </summary>
+    public class HomeController : ApiController
+    {
+        /// <summary>
+        /// 索引
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult Index()
+        {
+            return Ok(new Student() { Name = "Huayu", Number = "5554432" });
+        }
+    }
+
+    /// <summary>
+    /// 學生
+    /// </summary>
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+    }
+
+}
