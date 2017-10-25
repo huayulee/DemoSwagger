e3592695d5d7b8bdac4393cb9461106b9b485370
Huayu Lee <huayu.lee@gmail.com>
2017-10-26 00:21:34 +0800

    222

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Http.Results;
+using System.ServiceModel.Channels;
 
 namespace WebApplication1.Controllers
 {
@@ -15,15 +17,22 @@ namespace WebApplication1.Controllers
     /// </summary>
     public class HomeController : ApiController
     {
-        /// <summary>
-        /// 索引
-        /// </summary>
-        /// <returns></returns>
         [HttpGet]
         [ResponseType(typeof(Student))]
         public IHttpActionResult Index()
         {
             return Ok(@"{""Name"":""Huayu"",""Number"":""333333""}");
+        }
+
+        /// <summary>
+        /// 索引
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IHttpActionResult GetClientIP()
+        {
+            //return Ok(Request.GetOwinContext().Request.RemoteIpAddress);
+            return Ok(GetClientIp());
         }
 
         [HttpGet]
@@ -33,6 +42,29 @@ namespace WebApplication1.Controllers
             var jsonString = @"{""Name"":""Huayu"",""Number"":""333333""}";
             var obj = JsonConvert.DeserializeObject<Student>(jsonString);
             return Ok(obj);
+        }
+
+        private string GetClientIp(HttpRequestMessage request = null)
+        {
+            request = request ?? Request;
+
+            if (request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return ((HttpContextWrapper)request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            }
+            else if (request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty prop = (RemoteEndpointMessageProperty)request.Properties[RemoteEndpointMessageProperty.Name];
+                return prop.Address;
+            }
+            else if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.UserHostAddress;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 
